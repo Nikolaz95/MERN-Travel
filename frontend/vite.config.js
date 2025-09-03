ed01ccb1f6d57241f98c249d3fa67a019a7621d1
Nikola Zovko <nikolajoe95@gmail.com>
2025-09-03 21:34:49 +0200

    ngrokServerSetup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // da otvori i za vanjske requeste
+    port: 5173,
+    allowedHosts: ['endless-caiman-fully.ngrok-free.app']
+  }
 })
