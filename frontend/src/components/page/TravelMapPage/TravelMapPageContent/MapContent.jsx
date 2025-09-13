e9b1d737bf023d1e6d9e9af6f58e53e469ce0914
Nikola Zovko <nikolajoe95@gmail.com>
2025-09-14 01:45:11 +0200

    updateMapContent

--- frontend/src/components/page/TravelMapPage/TravelMapPageContent/MapContent.jsx
+++ frontend/src/components/page/TravelMapPage/TravelMapPageContent/MapContent.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate, useSearchParams } from "react-router";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 
 //import css
 import './MapContent.css';
@@ -7,20 +9,24 @@ import Button from '../../../layouts/Buttons/Button';
 
 const MapContent = () => {
     const navigate = useNavigate();
+    const [mapPosition, setMapPosition] = useState([40, 0]);
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
     return (
         <section className='mapSection' onClick={() => navigate("newVisiting")}>
-            <main className='mapMainContent'>
-                <h1>Map Section</h1>
-                <h3>Position: {lat}, {lng}</h3>
-                <Button onClick={() => {
-                    setSearchParams({ lat: 23, lng: 50 })
-                }}>
-                    Change Position
-                </Button>
-            </main>
+            {/* <main className='mapMainContent'> */}
+            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className='map'>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
+                <Marker position={mapPosition}>
+                    <Popup>
+                        DISI BIO 91 <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+            {/* </main> */}
         </section>
     )
 }
